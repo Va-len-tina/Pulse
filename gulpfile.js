6d53dc27e6393cd81d775de83a8b72a7e4465072
tina.jbit@gmail.com <tina.jbit@gmail.com>
2021-03-21 16:03:18 +0300

    add media

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const gulp = require('gulp');
+const gulp        = require('gulp');
 const browserSync = require('browser-sync');
 const sass = require('gulp-sass');
 const rename = require("gulp-rename");
@@ -15,23 +15,21 @@ gulp.task('server', function() {
 });
 
 gulp.task('styles', function() {
-    return gulp.src("src/sass/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({
-            prefix: "",
-            suffix: ".min",
-          }))
-        .pipe(autoprefixer({
-            cascade: false
-        }))
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
+    return gulp.src("src/sass/**/*.+(scss|sass)")
+            .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+            .pipe(rename({     
+                prefix: "",
+                suffix: ".min",
+              }))
+            .pipe(autoprefixer())
+            .pipe(cleanCSS({compatibility: 'ie8'}))
+            .pipe(gulp.dest("src/css"))
+            .pipe(browserSync.stream());
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/*.+(scss|sass)", gulp.parallel("styles"));
+    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
     gulp.watch("src/*.html").on("change", browserSync.reload);
-});
+})
 
 gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
